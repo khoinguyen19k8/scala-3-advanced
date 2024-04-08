1725cf728d28838805e12e48370d53d61d213cb5
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2024-04-08 15:15:24 +0200

    [afp] - Functional set part II with PBSet

--- src/main/scala/com/rockthejvm/practice/FunctionalSetPlayground.scala
+++ src/main/scala/com/rockthejvm/practice/FunctionalSetPlayground.scala
@@ -14,18 +14,53 @@ abstract class FSet[A] extends (A => Boolean) {
   def flatMap[B](f: A => FSet[B]): FSet[B]
   def filter(predicate: A => Boolean): FSet[A]
   def foreach(f: A => Unit): Unit
+
+  // methods
+  infix def -(elem: A): FSet[A]
+  infix def --(anotherSet: FSet[A]): FSet[A]
+  infix def &(anotherSet: FSet[A]): FSet[A]
+
+  // "negation" == all the elements of type A EXCEPT the elements in this set
+  def unary_! : FSet[A] = new PBSet(x => !contains(x))
 }
 
-case class Empty[A]() extends FSet[A] {
-   // TODO
+// example {x in N | x % 2 == 0}
+// property-based set
+class PBSet[A](property: A => Boolean) extends FSet[A] {
+  override def contains(elem: A): Boolean = property(elem)
+  override infix def +(elem: A): FSet[A] =
+    new PBSet(x => x == elem || property(x))
+  override infix def ++(anotherSet: FSet[A]): FSet[A] =
+    new PBSet(x => anotherSet(x) || property(x))
 
-  override def contains(elem: A): Boolean = false
-  override infix def +(elem: A): FSet[A] = Cons(elem, this)
-  override infix def ++(anotherSet: FSet[A]): FSet[A] = anotherSet
+  // "classics"
+  override def map[B](f: A => B): FSet[B] =
+    politelyFail()
+  override def flatMap[B](f: A => FSet[B]): FSet[B] =
+    politelyFail()
+  override def filter(predicate: A => Boolean): FSet[A] =
+    new PBSet(x => predicate(x) && property(x))
+  override def foreach(f: A => Unit): Unit =
+    politelyFail()
+
+  // methods
+  override infix def -(elem: A): FSet[A] =
+    filter(x => x != elem)
+  override infix def --(anotherSet: FSet[A]): FSet[A] =
+    filter(!anotherSet)
+  override infix def &(anotherSet: FSet[A]): FSet[A] =
+    filter(anotherSet)
+
+  // extra utilities (internal)
+  private def politelyFail() = throw new RuntimeException("I don't know if this set is iterable...")
+
+}
+
+case class Empty[A]() extends PBSet[A](_ => false) {
+  // TODO
 
   override def map[B](f: A => B): FSet[B] = Empty()
   override def flatMap[B](f: A => FSet[B]): FSet[B] = Empty()
-  override def filter(predicate: A => Boolean): FSet[A] = this
   override def foreach(f: A => Unit): Unit = ()
 }
 
@@ -48,6 +83,12 @@ case class Cons[A](head: A, tail: FSet[A]) extends FSet[A]{
   override def foreach(f: A => Unit): Unit =
     f(head)
     tail.foreach(f)
+
+  override infix def -(elem: A): FSet[A] = filter(_ != elem)
+
+  override infix def --(anotherSet: FSet[A]): FSet[A] = filter(!anotherSet)
+
+  override infix def &(anotherSet: FSet[A]): FSet[A] = filter(anotherSet)
 }
 
 object FSet {
@@ -70,6 +111,21 @@ object FunctionalSetPlayground {
     println(fooSet.map(_ * 2)(6)) // true
     println(fooSet.flatMap(x => FSet[Float](x.toFloat / 2))(1.5)) // true
     println(combinedSet.filter(_ % 2 == 0)(5)) // false
+
+    val aSet = Set(1,2,3)
+    val aList = (1 to 10).toList
+    println(aList.filter(aSet)) // List[1,2,3]
+
+    println((fooSet - 3)(3)) // false
+    println((fooSet -- anotherSet)(3)) // false
+    println((fooSet & anotherSet)(3)) // true
+    println((fooSet & anotherSet)(2)) // false
+
+    val naturals = new PBSet[Int](_ => true)
+    println(naturals.contains(5237548)) // true
+    println(!naturals.contains(0)) // false
+    println((!naturals + 1 + 2 + 3).contains(3)) // true
+//    println(!naturals.map(_ + 1)) // throw
 
   }
 }
